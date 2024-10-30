8abe06e0ab15438d0eb30b8d8517ee67d1d48a13
main_pc <my@vbaykov.ru>
2024-10-30 16:52:17 +0300

    Add func auth service

---
+++ app/auth/auth.py
@@ -0,0 +1,25 @@
+from pydantic import EmailStr
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import config
+from app.auth.schemas import EmailModel
+from app.auth.utils import verify_password
+from app.crud.crud import UserCRUD
+from app.db import SessionDep
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=30)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
+    return encode_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str, session: AsyncSession = SessionDep):
+    user = await UserCRUD.find_one_or_none(session=session, filters=EmailModel(email=email))
+    if not user or verify_password(plain_password=password, hashed_password=user.password) is False:
+        return None
+    return user
